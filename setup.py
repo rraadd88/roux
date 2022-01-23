b0a76240483a63c00cdb766e19e79e41818cc8e0
rraadd88 <rohanadandage@gmail.com>
2022-01-23 13:57:33 -0500

    added all to extra_require

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+"""
+## Development
+
+### release new version
+
+    git commit -am "version bump";git push origin master
+    python setup.py --version
+    git tag -a v$(python setup.py --version) -m "upgrage";git push --tags
+
+"""
+
 import sys
 if (sys.version_info[0]) != (3):
      raise RuntimeError('Python 3 required ')
@@ -7,30 +18,44 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-REQUIREMENTS =   """biopython>=1.71
-                    regex>=2018.7.11
-                    pandas>=0.25.3
-                    pyyaml>=5.1
-                    numpy>=1.17.3
-                    matplotlib>=2.2
-                    requests>=2.19.1
-                    scipy>=1.1.0
-                    tqdm>=4.38.0
-                    fastparquet
-                    pandarallel
-                    xlrd
-                    openpyxl
-                    argh
-                    icecream""".split('\n')
-    
-DEV_REQUIREMENTS = [
+## dependencies/requirements
+requirements = {
+'base':  """biopython>=1.71
+            regex>=2018.7.11
+            pandas>=0.25.3
+            pyyaml>=5.1
+            numpy>=1.17.3
+            matplotlib>=2.2
+            requests>=2.19.1
+            scipy>=1.1.0
+            tqdm>=4.38.0
+            fastparquet
+            pandarallel
+            xlrd
+            openpyxl
+            argh
+            icecream""".split('\n'),
+'stat':['statsmodels',
+        # 'pingouin',
+        ],
+'viz':['seaborn>=0.8',
+      ],
+'query':['pyensembl',
+         'pybiomart'],
+'workflow':['snakemake',
+            'gitpython'],
+'dev':[
     'black',
     'coveralls == 3.*',
     'flake8',
     'isort',
     'pytest == 6.*',
     'pytest-cov == 2.*',
-]
+],
+}
+extras_require={k:l for k,l in requirements.items() if not k=='base'}
+## all extra except dev
+extras_require['all']=list(set([l for _,l in extras_require.items() if not k=='dev']))
 
 setuptools.setup(
     name='roux',
@@ -47,19 +72,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=REQUIREMENTS,
-    extras_require={
-        'stat':['statsmodels',
-                # 'pingouin',
-                ],
-        'viz':['seaborn>=0.8',
-              ],
-        'query':['pyensembl',
-                 'pybiomart'],
-        'workflow':['snakemake',
-                    'gitpython'],
-        'dev': DEV_REQUIREMENTS,
-    },
+    install_requires=requirements['base'],
+    extras_require=extras_require,
     entry_points={
     'console_scripts': ['rohan = roux.run:parser.dispatch',],
     },    
