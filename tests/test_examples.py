5890b259942e10f13bbb90f0b8ac12b9f896ec6e
Rohan Dandadge <rohanadandage@gmail.com>
2022-11-11 16:17:13 -0500

    ADDED: test for encoding

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,10 @@
 from testbook import testbook
 
+@testbook('examples/roux_lib_str.ipynb', execute=True)
+def test_stdout(tb):
+    assert tb.cell_output_text('encoded_long')=='eNqVj00KwjAQRq8Ssqli8QCCK6_gTiSk7WcJNkmZSbRF9OwmjYtuhSwm7_HNz0u2fjCuwyQPQnYUe2E6WYuMWdtxQOalWpnYMMLK_ECxcxY6tvl782TjoDmhV2biI06bElIlVIszQQcLFzaEGwiuxbFKZbXdip0YyVhNs_KkLILm9ExuJ62Z0A1WvtOY-5NVj6CSDawIPYHZeLeM7cnHcYlwS4BT6Y4cemgyuikX_rPU5bwP4HCV7y_fP20r', 'possible change in the funtion.'
+    assert tb.cell_output_text('encoded_short')=='e11fafe6bf21d3db843f8a0e4cea21bc600832b3ed738d2b09ee644ce8008e44', 'possible change in the funtion.'
+    
 @testbook('examples/roux_lib_df.ipynb', execute=True)
 def test_stdout(tb):
     # assert tb.cell_output_text(1) == 'hello world!'
@@ -24,4 +29,4 @@ def test_stdout(tb):
 
 @testbook('examples/roux_viz_io.ipynb', execute=True)
 def test_stdout(tb):
-    assert "title={'center':'modified'}" in tb.cell_output_text('read_plot_modified')    
+    assert "title={'center':'modified'}" in tb.cell_output_text('read_plot_modified')
