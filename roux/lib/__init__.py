3910b83cf60c11c6002a49a273a8a32c4b2d7b3b
rraadd88 <rohanadandage@gmail.com>
2024-08-06 21:46:28 -0400

    ruff df.py

--- roux/lib/__init__.py
+++ roux/lib/__init__.py
@@ -1,39 +1,43 @@
 import pandas as pd
 
+
 def to_class(cls):
-    """Get the decorator to attach functions. 
-    
+    """Get the decorator to attach functions.
+
     Parameters:
         cls (class): class object.
 
     Returns:
         decorator (decorator): decorator object.
-    
+
     References:
         https://gist.github.com/mgarod/09aa9c3d8a52a980bd4d738e52e5b97a
     """
     from functools import wraps
+
     def decorator(func):
-        @wraps(func) 
-        def wrapper(self, *args, **kwargs): 
-            return func(self._obj,*args, **kwargs)
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            return func(self._obj, *args, **kwargs)
+
         setattr(cls, func.__name__, wrapper)
         return func
+
     return decorator
+
 
 @pd.api.extensions.register_dataframe_accessor("rd")
 class rd:
-    """`roux-dataframe` (`.rd`) extension.
-    """
+    """`roux-dataframe` (`.rd`) extension."""
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
+
 # create the `roux-dataframe` (`.rd`) decorator
-to_rd=to_class(rd)
+to_rd = to_class(rd)
 
 # @pd.api.extensions.register_dataframe_accessor("stat")
 # class stat:
 #     def __init__(self, pandas_obj):
 #         self._obj = pandas_obj
-
-
--- roux/run.py
+++ roux/run.py
@@ -1,9 +1,11 @@
 """For access to a few functions from the terminal."""
+
 import logging
+
 logging.getLogger().setLevel(logging.INFO)
 
 import argh
-from roux.lib.io import backup,to_version,to_zip
+from roux.lib.io import backup, to_version, to_zip
 from roux.lib.sys import read_ps
 from roux.workflow.io import read_config, read_metadata, replacestar, to_clean_nb
 from roux.workflow.task import run_tasks
@@ -11,24 +13,26 @@ from roux.workflow.nb import to_clear_unused_cells, to_clear_outputs
 
 ## begin
 parser = argh.ArghParser()
-parser.add_commands([
-    ## io
-    read_ps,
-    ## backup
-    backup,
-    to_version,
-    to_zip,
-    ## workflow io
-    read_config,
-    read_metadata,
-    ## workflow execution
-    run_tasks,
-    ## notebook post-processing
-    replacestar,
-    to_clear_unused_cells,
-    to_clear_outputs,
-    to_clean_nb, ## wrapper for above
-])
+parser.add_commands(
+    [
+        ## io
+        read_ps,
+        ## backup
+        backup,
+        to_version,
+        to_zip,
+        ## workflow io
+        read_config,
+        read_metadata,
+        ## workflow execution
+        run_tasks,
+        ## notebook post-processing
+        replacestar,
+        to_clear_unused_cells,
+        to_clear_outputs,
+        to_clean_nb,  ## wrapper for above
+    ]
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser.dispatch()
