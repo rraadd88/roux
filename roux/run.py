ac3aa2873f7537196974ea3baffaf3ff2dfbb2c4
rraadd88 <rohanadandage@gmail.com>
2024-12-23 11:15:33 -0500

    updates

--- roux/run.py
+++ roux/run.py
@@ -15,12 +15,36 @@ from roux.workflow.task import (
 )
 from roux.workflow.nb import to_clear_unused_cells, to_clear_outputs
 
+def head_table(
+    p : str,
+    n : int = 5,
+    use_dir_paths : bool = True,
+    not_use_dir_paths : bool = False,
+    use_paths : bool = True,
+    not_use_paths : bool = False,
+    **kws,
+    ):
+    if not_use_dir_paths: 
+        use_dir_paths=False
+    if not_use_paths: 
+        use_paths=False
+        
+    from roux.lib.io import read_table
+    return read_table(
+        p,
+        use_dir_paths=use_dir_paths,
+        use_paths=use_paths,
+        **kws,
+    ).head(n)
+    
 ## begin
 parser = argh.ArghParser()
 parser.add_commands(
     [
         ## io
         read_ps,
+        ## checks
+        head_table,
         ## backup
         backup,
         to_version,
