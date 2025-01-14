8019da6db26c1e4451a506c30308a428e5c85fe4
rraadd88 <rohanadandage@gmail.com>
2025-01-13 20:45:43 -0500

    amend

--- roux/workflow/task.py
+++ roux/workflow/task.py
@@ -284,6 +284,9 @@ def run_tasks(
     #     clean=False
 
     ## run tasks
+    ## for log
+    from datetime import datetime
+    _start_time = datetime.now()
 
     df1 = (
         pd.Series(parameters_list)
@@ -354,7 +357,9 @@ def run_tasks(
                 cpus=fast_workers,
             )
             )
-        
+            
+    ## log
+    logging.info(f"Time taken: {datetime.now()-_start_time}")
     if not out_paths:
         return before
     else:
